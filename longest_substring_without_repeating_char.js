85bb70059dc28e05dc31d4c789266d45d611be6d
Rohan Paul <rohanpaul2@gmail.com>
2018-08-09 01:24:50 +0530

    longest substring

--- longest_substring_without_repeating_char.js
+++ longest_substring_without_repeating_char.js
@@ -22,18 +22,51 @@ And then push this char at this index position to the resetted-newSubStr value.
 C> And after all the above, at each iteration of the loop, we are reassigning the maxLen to be the max of maxLen till now and subArr.length. And by the end of all the iterations I am returning the final max.
 */
 
-var lengthOfLongestSubstring = function (s) { 
+// SOLUTION-1
+var lengthOfLongestSubstring = function (s) {
+
   let newSubStr = [], maxLen = 0, index;
 
   for (let i = 0; i < s.length; i++) {
+
     index = newSubStr.indexOf(s[i]);
+
     if (index !== -1) {
-      newSubStr = newSubStr.slice(index+1, newSubStr.length); 
-  }
+
+        newSubStr = newSubStr.slice(index+1);  //Note, for slice() If endIndex is omitted, slice() extracts to the end of the string, when I want to extract / slice till the end of the string, I dont need to pass the second argument.
+   }
+
   newSubStr.push(s[i]);
-  maxLen = Math.max(maxLen, newSubStr.length); 
-  }
+
+  maxLen = Math.max(maxLen, newSubStr.length);
+
+ }
+
   return maxLen;
 }
 
-console.log(lengthOfLongestSubstring("abcabcbb"));
+// console.log(lengthOfLongestSubstring("abcabcbb"));   // => 3
+// console.log(lengthOfLongestSubstring("bbbbb"));   // => 1
+// console.log(lengthOfLongestSubstring("pwwkew"));   // => 3
+
+// SOLUTION-2 - The same exact logic, only difference is, I am not using a separate variable index here. In the above I am first checking if this item is duplication (if duplicate then fist slice) and only then I am pushing to the newSubStr the char. But here, I am combining both these steps in below.
+
+lengthOfLongestSubstring_2 = s => {
+
+    let max = 0, newSubStr = [];
+
+    for (let i = 0; i < s.length; i++) {
+
+        // If 'i' is duplicated newSubStr.indexOf(i) will return the current index of that i and I will start slicing from the next position. And if its not duplicated, I will staring slicing from 0-index position and all the way upto the last char of newSubStr.
+
+        newSubStr = newSubStr.slice(newSubStr.indexOf(s[i]) + 1)
+        max = Math.max(newSubStr.push(s[i]), max);  // remember the push() returns the new length of the array after pushing
+    }
+    return max;
+
+}
+
+
+console.log(lengthOfLongestSubstring_2("abcabcbb"));   // => 3
+console.log(lengthOfLongestSubstring_2("bbbbb"));   // => 1
+console.log(lengthOfLongestSubstring_2("pwwkew"));   // => 3
